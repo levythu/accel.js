ea36c4433d64f0ca2bc4779b6f7e4ffac56e2be5
Levy <zly.george@163.com>
2017-04-15 16:55:24 -0400

    delta data sync

--- test/simpletest.js
+++ test/simpletest.js
@@ -1,24 +1,29 @@
 var accel=require("../lib/index");
 var $=accel();
 
-accel.init({
-    env: [
-        {
-            local: 1,
-        },
-    ],
-}, function(err) {
+accel.init({}, function(err) {
     if (err) {
         console.log(err);
         return;
     }
     $.a=0.5;
-    $(function add(a, b) {
-        return a*$.a+b*(1-$.a);
+    $.rand=function(next) {
+        next = next * 1103515245 + 12345;
+        return Math.floor(next/65536) % 32768;
+    }
+    $(function calculate(q, times) {
+        for (var i=0; i<times; i++) q=$.rand(q);
+        return q;
     });
 
-    $.add(10, 4, function(result) {
-        console.log(result);    // 7
+    $.calculate(1, 10000000, function(result) {
+        console.log("Seed: 1, Times: 10M, Res:", result);
+    });
+    $.calculate(2, 10000000, function(result) {
+        console.log("Seed: 2, Times: 10M, Res:", result);
+    });
+    $.calculate(3, 10000000, function(result) {
+        console.log("Seed: 3, Times: 10M, Res:", result);
     });
 
 });
