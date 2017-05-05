59fe40c74b1b15dfa21730abdcfe8bb31cc5b530
Levy <zly.george@163.com>
2017-05-05 09:27:36 -0400

    acceljs is capable of loading configure

--- bin/launcher.js
+++ bin/launcher.js
@@ -2,12 +2,20 @@
 
 const path=require("path");
 const accel=require("accel");
+const fs=require("fs");
 
 process.argv.shift();
 process.argv.shift();
 var targetJS=path.join(process.cwd(), process.argv[0]);
 process.argv.unshift("nodejs");
 
-accel.init({}, () => {
+var conf={};
+try {
+    rs=fs.readFileSync("accel-config.json", {encoding: 'utf8'});
+    conf=JSON.parse(rs);
+} catch (e) {
+}
+
+accel.init(conf, () => {
     require(targetJS);
 });
