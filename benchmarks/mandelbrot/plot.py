50c85ba03a0fb3029d413aa5f700e5ef3788e2bc
harlenVII <harlanxu16@gmail.com>
2017-05-09 21:20:01 -0400

    new result

--- benchmarks/mandelbrot/plot.py
+++ benchmarks/mandelbrot/plot.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
-plt.rcdefaults()
 import numpy as np
-import matplotlib.pyplot as plt
 
-
-plt.rcdefaults()
 fig, ax = plt.subplots()
 people = ('Serial', 'Parallel.js', 'Accel-1', 'Accel-2', 'Accel-4')
 y_pos = np.arange(len(people))
-performance = [32.2568,18.3980,9.67925,5.1116,3.5146]
-ax.barh(y_pos, performance, align='center',
-        color='green', ecolor='black')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Execution time (s)')
+overhead = [32.2568, 19.449, 9.644 ,5.057 ,3.5146]
+compute = [32.2568, 9.721, 9.37325, 4.67075, 2.3064]
+for i in range(len(overhead)):
+    overhead[i] -= compute[i]
+ax.bar(y_pos, compute, align='center', color='green', ecolor='black', label='compute')
+ax.bar(y_pos, overhead, align='center', color='red', ecolor='black', bottom=compute, label='overhead')
+ax.set_xticks(y_pos)
+ax.set_xticklabels(people)
+ax.set_ylabel('Execution time (s)')
 ax.set_title('Mandelbrot')
-
+legend = ax.legend(loc='upper right', shadow=True)
 plt.savefig('mandelbrot.png')
