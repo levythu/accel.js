617ee4057af578acaef096dfbd3c2e1be01e711c
Levy <zly.george@163.com>
2017-05-21 14:40:30 -0400

    remove benchmark from master branch

--- benchmarks/fontserver/server/run.js
+++
@@ -1,9 +0,0 @@
-var app = require('./app');
-var http = require('http');
-
-app.set('port', 2333);
-
-
-app.listen(2333, function() {
-    console.log('Express server listening on port ' + 2333);
-});
--- benchmarks/timing.js
+++
@@ -1,9 +0,0 @@
-module.exports=function time(func, times=1) {
-    var now=(new Date()).getTime();
-    for (var i=0; i<times; i++) {
-        func();
-    }
-    var after=(new Date()).getTime();
-    console.log("Running", times, "times with time", after-now, "ms");
-    return after-now;
-}
